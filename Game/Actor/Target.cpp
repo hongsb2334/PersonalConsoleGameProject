fee6d9dac94e1b7e77cccf160ceca3e0d5fbb847
hongsb2334 <hongsb2334@gmail.com>
2026-08-26 19:00:17 +0900

    Game,Shooting 폴더 삭제

--- Game/Actor/Target.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "Target.h"
-using namespace Craft;
-
-Target::Target(const Vector2& position) : Actor("T", position, Color::Yellow)
-{
-	//우선순위 설정
-	//바닥보다는 크고 박스 및 플레이어는 작은 값
-	sortingOrder = 3;
-}
