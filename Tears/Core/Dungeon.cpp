0e5c7820a1a592e182a1a5e90399eec10ee8e064
hongsb2334 <hongsb2334@gmail.com>
2026-09-02 19:05:55 +0900

    단일 방 생성 구현

---
+++ Tears/Core/Dungeon.cpp
@@ -0,0 +1,141 @@
+﻿#include "Dungeon.h"
+#include <queue>
+#include <Util/Util.h>
+#include <algorithm>
+bool DungeonMap::GenerateRoom(int roomNum, int maxRegenerateNum)
+{
+    return false;
+}
+
+RoomNode* DungeonMap::FindRoom(const GridCoord& coord)
+{
+    return nullptr;
+}
+
+GridCoord DungeonMap::GetOffset(EntryDirection entry)
+{
+    GridCoord offset;
+    switch (entry)
+    {
+    case EntryDirection::None:
+        break;
+    case EntryDirection::Top:
+        offset.y = -1;
+        break;
+    case EntryDirection::Bottom:
+        offset.y = 1;
+        break;
+    case EntryDirection::Left:
+        offset.x = -1;
+        break;
+    case EntryDirection::Right:
+        offset.x = 1;
+        break;
+    default:
+        break;
+    }
+
+    return offset;
+}
+
+bool DungeonMap::GenerateRoomOnce(int roomNum)
+{
+    //이전 데이터 지움
+    Clear();
+    //BFS로 방 생성 위해 큐 생성
+    std::queue<RoomNode*> roomQueue;
+
+    //시작방 생성, (0, 0)에서 시작
+    this->startRoom = &dungeon[GridCoord(0, 0)];
+    startRoom->occupied = true;
+    startRoom->distance = 0;
+    roomQueue.push(startRoom);
+    roomCount = 1;
+
+    RoomNode* current = startRoom;
+    
+    while (!roomQueue.empty())
+    {
+        //방이 최소 한번은 확장했는지 플래그
+        bool isExpanded = false;
+        //랜덤으로 네 방향을 순회하기 위해 배열 선언후 방향 넣어서 초기화
+        std::vector<EntryDirection> randomDirection = {EntryDirection::Top, EntryDirection::Right, EntryDirection::Bottom, EntryDirection::Left };
+        
+        //shuffle에 배열 iterator와 이미 만들어놓은 GetRandomEngine 넣어서 배열 섞어줌
+        std::shuffle(randomDirection.begin(), randomDirection.end(), Util::GetRandomEngine());
+        
+        current = roomQueue.front();
+        roomQueue.pop();
+        
+        for (EntryDirection entry : randomDirection)
+        {
+            //이미 인자로 받은 방 갯수만큼 모두 생성했을 경우 break
+            if (roomCount == roomNum)
+            {
+                break;
+            }
+
+            //방 방향에 맞는 offset값 받아서             
+            GridCoord offset = GetOffset(entry);
+            GridCoord newCoord = current->coord + offset;
+            RoomNode& newRoom = dungeon[newCoord];
+            //새로 생긴 노드는 위치가 (0, 0)으로 초기화되기 때문에 직접 넣어줘야 함
+            newRoom.coord = newCoord;
+            
+            //방 생성했는데 방이 이미 생성되어 있는 좌표에 생성했으면 continue
+            if (newRoom.occupied)
+            {
+                continue;
+            }
+
+            //50%의 확률로 continue
+            if (Util::RandomRange(0, 1))
+            {
+                continue;
+            }
+
+            //여기까지 통과했으면 방 생성
+            newRoom.occupied = true;
+            newRoom.distance = current->distance + 1;
+            //방 연결
+            Connect(current, &newRoom, entry);
+            roomQueue.push(&newRoom);
+            roomCount++;
+            isExpanded = true;
+
+        }
+        
+        if (!isExpanded)
+        {
+            endRoomList.push_back(current);
+        }
+    }
+
+
+
+    //보스방이 시작 방과 바로 붙어있는지 확인
+    if (roomCount != roomNum)
+    {
+        return false;
+    }
+
+    //끝 방 리스트의 마지막 항복을 bossRoom으로 지정
+    if (endRoomList.empty())
+    {
+        return false;
+    }
+    bossRoom = endRoomList.back();
+    return true;
+}
+
+void DungeonMap::Clear()
+{
+
+
+
+}
+
+bool DungeonMap::Connect(RoomNode* room1, RoomNode* room2, EntryDirection entryDirection)
+{
+    return false;
+}
--- Tears/Core/Dungeon.h
+++ Tears/Core/Dungeon.h
@@ -1,5 +1,8 @@
 ﻿#pragma once
 #include <unordered_map>
+#include <vector>
+#include "EntryDirection.h"
+
 //던전 평면도에서의 좌표
 struct GridCoord
 {
@@ -11,8 +14,8 @@ struct GridCoord
         return ((x == other.x) && (y == other.y));
     }
 
-    GridCoord operator+(const GridCoord& other)
-    {
+    GridCoord operator+(const GridCoord& other) const
+    {   
         return GridCoord(x + other.x, y + other.y);
     }
 
@@ -23,19 +26,67 @@ struct GridCoord
 // 해시 테이블, 던전 평면도 좌표를 전달받아서 해시맵의 키 형태로 리턴
 struct GridHash 
 {
-    size_t operator()(const GridCoord& grid)
+    size_t operator()(const GridCoord& grid) const
     {
         //해시값 리턴(정수형)
         return std::hash<int>()(grid.x) * 31 ^ std::hash<int>()(grid.y);
     }
 };
 
+//던전의 방 하나
 struct RoomNode
 {
     GridCoord coord;
-    RoomNode* topRoom;
-    RoomNode* leftRoom;
-    RoomNode* rightRoom;
-    RoomNode* bottomRoom;
+    RoomNode* topRoom = nullptr;
+    RoomNode* leftRoom = nullptr;
+    RoomNode* rightRoom = nullptr;
+    RoomNode* bottomRoom = nullptr;
+
+    //
+    bool occupied = false;
+    //방과 방끼리의 거리, -1로 초기화한것은 아직 값이 할당되지 않았음을 의미
+    int distance = -1 ;
+};
+
+class DungeonMap
+{
+public:
+    //방 생성하는 함수, 최대 방 개수와 최대 재시도 횟수를 인자로 받는다.
+    bool GenerateRoom(int roomNum, int maxRegenerateNum);
+
+    //좌표로 노드 찾는 함수
+    RoomNode* FindRoom(const GridCoord& coord);
+    const std::vector<RoomNode*>& GetEndRoomList() const { return endRoomList; }
+
+    //시작방/보스방 꺼내는 함수
+    RoomNode* GetStartRoom() const { return startRoom; }
+    RoomNode* GetBossRoom() const { return bossRoom; }
+
+private:
+    //방 생성 한번 시도 함수
+    bool GenerateRoomOnce(int roomNum);
+    
+    //이전 데이터 지우는 함수
+    void Clear();
+    
+    //두 방 노드들을 연결하는 함수
+    bool Connect(RoomNode* room1, RoomNode* room2, EntryDirection entryDirection);
+
+    //랜덤 방 방향 받아서 그에 맞는 좌표 offset 값 리턴하는 함수
+    GridCoord GetOffset(EntryDirection entry);
+
+private:
+    //던전
+    std::unordered_map<GridCoord, RoomNode, GridHash> dungeon;
+    
+    //시작방과 보스방 가리키는 포인터
+    RoomNode* startRoom = nullptr;
+    RoomNode* bossRoom = nullptr;
+
+    //방 개수 세는 변수
+    int roomCount = 0;
+    
+    //더이상 확장 못하는 끝방을 담을 배열
+    std::vector<RoomNode*> endRoomList;
 
 };
